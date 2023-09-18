a8b7dfe7d330e961d85c47c9c72ae1649adf5774
Navjot <hanspalnavjot555@gmail.com>
2023-09-18 16:18:17 +0530

    this is a to-do-List_app

---
+++ app/src/main/java/com/example/to_do_list/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.to_do_list
+
+import android.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var item :TextView
+    lateinit var  add:Button
+    lateinit var listView: ListView
+    var itemList=ArrayList<String>()
+    var fileHelper=FileHelper()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        item= findViewById(R.id.addtext)
+        add= findViewById(R.id.enter)
+        listView= findViewById( R.id.list)
+
+        itemList = fileHelper.readData(this)
+
+        var arrayAdapter= ArrayAdapter(this, android.R.layout.simple_list_item_1,android.R.id.text1,itemList)
+        listView.adapter= arrayAdapter
+
+
+        add.setOnClickListener {
+            var itemName:String= item.text.toString()
+            itemList.add(itemName)
+            item.setText("")
+            fileHelper.writeData(itemList,applicationContext)
+            arrayAdapter.notifyDataSetChanged()
+        }
+
+        listView.setOnItemClickListener { adapterView, view, position, l ->
+
+            var alert= AlertDialog.Builder(this)
+                alert.setTitle("Delete")
+            alert.setMessage("Do you want to delete item from the a List ")
+            alert.setCancelable(false)
+            alert.setNegativeButton("No") { dialogInterface, i ->
+                dialogInterface.cancel()
+            }
+
+            alert.setPositiveButton("Yes") { dialogInterface, i ->
+
+                itemList.removeAt(position)
+                arrayAdapter.notifyDataSetChanged()
+                fileHelper.writeData(itemList, applicationContext)
+            }
+            alert.create().show()
+        }
+    }
+}
